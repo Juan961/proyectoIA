626f29a975595cb9755885906e57be6846079098
Juan Jose <juanjo5792@gmail.com>
2021-05-28 13:05:17 +0000

    Version 0.7 - No funcional

--- scripts/index.js
+++ scripts/index.js
@@ -29,60 +29,25 @@ boton.addEventListener('click', () => {
                 {
                     "language": "es",
                     "id": "2",
-                    "text": materiaFisica
+                    "text": materiaMatematicas
                 },
                 {
                     "language": "es",
                     "id": "3",
-                    "text": materiaQuimica
+                    "text": materiaIngles
                 },
                 {
                     "language": "es",
                     "id": "4",
-                    "text": materiaMatematicas
+                    "text": materiaQuimica
+                },
+                {
+                    "language": "es",
+                    "id": "5",
+                    "text": materiaFisica
                 }
             ]
         }
     }).then(res=>console.log(res))
 })
 
-/*
-var body = {
-    "documents": [
-        {
-            "language": "es",
-            "id": "1",
-            "text": materiaEspañol
-        },
-        {
-            "language": "es",
-            "id": "2",
-            "text": materiaFisica
-        },
-        {
-            "language": "es",
-            "id": "3",
-            "text": materiaQuimica
-        },
-        {
-            "language": "es",
-            "id": "4",
-            "text": materiaMatematicas
-        }
-    ]
-}
-
-var direccion = 'https://servicioanalisistexto.cognitiveservices.azure.com/text/analytics/v3.0/sentiment'
-
-axios.post(direccion, body, {
-    headers:{
-        'Ocp-Apim-Subscription-Key': '8b4c7829af844bc699425c01e131246a',
-        'Content-Type': 'application/json'
-    }
-})
-.then(repuesta => {
-    console.log(repuesta.data)
-})
-.catch(error => console.log(error))
-
-*/
